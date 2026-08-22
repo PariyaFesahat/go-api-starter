ffb2344b4a4aa95a5f840e5ca72b9d0ba4028c9e
pariyafesahat <pariyafesahat@gmail.com>
2026-08-22 10:29:15 +0330

    db GET users

--- database/database.go
+++ database/database.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/pariyafesahat/go-rest-api/models"
 )
 
 // connString := "postgres://postgres:admin@localhost:5432/go_rest_api"
+
 func Connect() (*pgx.Conn, error) {
 	conn, err := pgx.Connect(
 		context.Background(),
@@ -17,4 +19,40 @@ func Connect() (*pgx.Conn, error) {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 	return conn, nil
+}
+
+func GetUsers(conn *pgx.Conn) ([]models.User, error) {
+	rows, err := conn.Query(context.Background(), `
+		SELECT id, name, email, age
+		FROM users
+		ORDER BY id
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query users: %w", err)
+	}
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+
+		err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Email,
+			&user.Age,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan user: %w", err)
+		}
+
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error while reading users: %w", err)
+	}
+
+	return users, nil
 }
